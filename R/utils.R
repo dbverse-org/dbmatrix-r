9a6d07babd68ff2f4db38db15a2bc0199a192d62
Ed2uiz <ecr7407@gmail.com>
2025-09-23 16:12:23 -0400

    refactor: update `unique_table_name` function to use default prefix "dbMatrix" --- TODO: move to dbProject/dbData

--- R/utils.R
+++ R/utils.R
@@ -351,7 +351,7 @@ setMethod(
 #'
 #' @noRd
 #' @keywords internal
-unique_table_name <- function(prefix = ""){
+unique_table_name <- function(prefix = "dbMatrix"){
   vals <- c(letters, LETTERS, 0:9)
   name <- paste0(sample(vals, 10, replace = TRUE), collapse = "")
   paste0(prefix, "_", name)
